c4aeb9c3434b3ef43f8ee58becdc932071f31af6
sakthivel1507 <98800423+sakthivel1507@users.noreply.github.com>
2026-04-17 15:25:25 +0530

    Update response message in root route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Github Action CI/CD Pipeline"});
+  res.status(200).json({ status: "ok", message: "Github"});
 });
 
 app.get("/health", (req, res) => {
